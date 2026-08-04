465d30d0d6563493d2f5b62a5183671672081f49
alejandro guttero <aguttero@gmail.com>
2026-08-04 13:55:13 -0400

    0804 custom validator

--- src/main.py
+++ src/main.py
@@ -142,6 +142,62 @@ async def read_validate_items(q: Annotated[str | None, Query(min_length=3, max_l
     return results
 
 
+@app.get("/items4/")
+async def read_items_add_title(
+    q: Annotated[str | None, Query(title="Query string", description="Query string for the items to search in the database that have a good match", min_length=3)] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items5/")
+async def read_items_alias_deprecated(
+    q: Annotated[
+        str | None,
+        Query(
+            alias="item-query",
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+            max_length=50,
+            pattern="^fixedquery$",
+            deprecated=True,
+        ),
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+# CUSTOM VALIDATION
+from pydantic import AfterValidator
+import random
+
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
+
+
+def check_valid_id(id: str):
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+    return id
+
+
+@app.get("/items6/")
+async def read_items_custom_validator(
+    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
+):
+    if id:
+        item = data.get(id)
+    else:
+        id, item = random.choice(list(data.items()))
+    return {"id": id, "name": item}
+
 
 
 # VALIDATION for

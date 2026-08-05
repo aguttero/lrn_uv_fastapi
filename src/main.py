e5940249c0a20d3bbb64f3356c27255cc25d494d
aguttero <aguttero@gmail.com>
2026-08-05 18:53:23 -0400

    0805 response model return type

--- src/main.py
+++ src/main.py
@@ -1,248 +1,38 @@
-from fastapi import FastAPI, HTTPException
-import sys
-# import os
+from typing import Annotated
+
+from fastapi import Cookie, FastAPI
 
 app = FastAPI()
 
-@app.get ("/")
-async def root():
-    return {"message": "Hello World! This is Panorámix"}
 
-@app.get("/getpythonversion")
-async def get_python_version():
-    return {"version": sys.version}
+@app.get("/items1/")
+async def read_items1(ads_id: Annotated[str | None, Cookie()] = None):
+    return {"ads_id": ads_id}
 
-# @app.get("/getenv")
-# def get_all_env_vars():
-#     # Converts the environment mapping into a standard Python dictionary
-#     return dict(os.environ)
+from fastapi import Header
+@app.get("/items2/")
+async def read_items_2(user_agent: Annotated[str | None, Header()] = None):
+    print (type(user_agent))
+    print (user_agent)
+    return {"User-Agent": user_agent}
 
-# @app.get("/getenv/{var_name}")
-# def get_specific_env_var(var_name: str):
-#     value = os.getenv(var_name)
-
-    # if value is None:
-        # raise HTTPException(status_code=404, detail="Environment variable not found")
-
-    # return {var_name: value}
-
-@app.get("/intitems/{item_id}")
-async def read_int_item(item_id:int):
-    return {"item_id": item_id}
-
-@app.get("/stringitem/{item_id}")
-async def read_str_item(item_id:str):
-    return {"item_id": item_id}
-
-# Predefined Path Values
-from enum import Enum
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-# FILE PATHS in PATH Parameter
-# Documentation will tell is a regular path parameeter
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
-
-# QUERY Parameters
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-@app.get("/slice_items/")
-async def slice_list_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
-
-# QUERY OPTIONAL parameter
-@app.get("/opt_par_items/{item_id}")
-async def read_item_and_opt_param(item_id: str, q: str | None = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
-
-@app.get("/items2/{item_id}")
-async def read_item_bool(item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-# MULTIPLE Path and Query Parameters
-@app.get("/users/{user_id}/items/{item_id}")
-async def read__multiple_items(user_id: int, item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-# Request BODY
 from pydantic import BaseModel
-
 class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
     tax: float | None = None
-
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.model_dump()
-    if item.tax is not None:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    print (f"Item type= {type(item)}")
-    print (f"Item= {item!r}")
-    print (f"Item name = {item.name!r}")
-    return item
-
-# UPDATE
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.model_dump()}
-    if q:
-        result.update({"q": q})
-    return result
-
-# VALIDATION: Query parameters and string and regular expressions
-# import ANNOTATED, QUERY
-from typing import Annotated
-from fastapi import Query
-
-@app.get("/items3/")
-async def read_validate_items(q: Annotated[str | None, Query(min_length=3, max_length=15,  pattern="^fixedquery$")] = None): # =Nonoe is the default value
-    results = {"things": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        print(results['things'])
-        print("---")
-        print(results.values())
-        results['things'].append({"q": q})
-        results.update ({"q":q})
-        # results.items.append({"q": q})
-    return results
-
-
-@app.get("/items4/")
-async def read_items_add_title(
-    q: Annotated[str | None, Query(title="Query string", description="Query string for the items to search in the database that have a good match", min_length=3)] = None,
-):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
-
-@app.get("/items5/")
-async def read_items_alias_deprecated(
-    q: Annotated[
-        str | None,
-        Query(
-            alias="item-query",
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
-            max_length=50,
-            pattern="^fixedquery$",
-            deprecated=True,
-        ),
-    ] = None,
-):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
-
-# CUSTOM VALIDATION
-from pydantic import AfterValidator
-import random
-
-data = {
-    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
-    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
-    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
-}
-
-
-def check_valid_id(id: str):
-    if not id.startswith(("isbn-", "imdb-")):
-        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
-    return id
-
-
-@app.get("/items6/")
-async def read_items_custom_validator(
-    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
-):
-    if id:
-        item = data.get(id)
-    else:
-        id, item = random.choice(list(data.items()))
-    return {"id": id, "name": item}
-
-# VALIDATION for
-# Query parameter Query()
-# Path paramenter Path()
-# Body contente Body()
-# Header content Header()
-# Cookie content Cookie()
-
-# PATH VALIDATION
-from fastapi import Path
-
-@app.get("/items7/{item_id}")
-async def read_items_path_validation(
-    item_id: Annotated[int, Path(title="The ID of the item to get", description="The path description")],
-    q: Annotated[str | None, Query(alias="item-query")] = None,
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    return results
-
-# Query Parameters with Pydantic Model
-from typing import Literal
-from pydantic import Field
-
-class FilterParams(BaseModel):
-    model_config = {"extra":"forbid"} # returns error if additional q parameters are added in the URL
-
-    limit: int = Field(100, gt=0, le=100)
-    offset: int = Field(0, ge=0)
-    order_by: Literal["created_at", "updated_at"] = "created_at"
     tags: list[str] = []
 
 
-@app.get("/items8/")
-async def read_items_pydnt_mod(filter_query: Annotated[FilterParams, Query()]):
-    return filter_query
-
-# Multiple Body Parameters
-
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
+@app.post("/items/")
+async def create_item(item: Item) -> Item:
+    return item
 
 
-@app.put("/items9/{item_id}")
-async def update_item_two_bodies(item_id: int, item: Item, user: User | None):
-    results = {"item_id": item_id, "item": item, "user": user}
-    return results
+@app.get("/items/")
+async def read_items() -> list[Item]:
+    return [
+        Item(name="Portal Gun", price=42.0),
+        Item(name="Plumbus", price=32.0),
+    ]
